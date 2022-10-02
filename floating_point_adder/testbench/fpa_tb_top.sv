e212e803fa1eba3e8480e4f49f29a05ec12cde2b
UandersonSilva <uandersons753@gmail.com>
2022-10-01 22:32:44 -0300

    plusargs TEST added.

--- floating_point_adder/testbench/fpa_tb_top.sv
+++ floating_point_adder/testbench/fpa_tb_top.sv
@@ -15,12 +15,43 @@ module fpa_tb_top;
         .underflow_out(fpa_if.underflow_out)
     );
 
-    initial_test t0;
-
     initial
     begin
-        t0 = new();
+        string test_name;
+
+        base_test t0;
+        
+        $value$plusargs("TEST=%s", test_name);
+
+        case(test_name)
+            "INITIAL_TEST":
+            begin
+                initial_test selected;
+                selected = new();
+                t0 = selected;
+            end
+            
+            "RANDOM_TEST":
+            begin
+                random_test selected;
+                selected = new();
+                t0 = selected;
+            end
+
+            default:
+            begin
+                random_test selected;
+                selected = new();
+                t0 = selected;
+                test_name = $sformatf("DEFAULT(RANDOM_TEST)");
+            end
+        endcase
+
         t0.env_r.fpa_if = fpa_if;
+
+        $display("%c[2;35m", 27, "[TB_TOP] Running %s", test_name);
+        $display("%c[0m", 27);
+        
         t0.run();
         #2;
         $finish;
