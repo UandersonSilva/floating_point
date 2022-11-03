7df0e8d67219166499ecf5546559a2fa0d3743f4
UandersonSilva <uandersons753@gmail.com>
2022-11-03 09:38:10 -0300

    random and overflow/underflow tests added.

---
+++ floating_point_divider/testbench/tb_classes/tests/ofuf_test.svh
@@ -0,0 +1,41 @@
+class ofuf_test extends base_test;
+    task run;
+        super.run();
+      
+        repeat(5)//underflow
+        begin            
+            data_i = new();
+
+            data_i.a_in[MANTISSA_WIDTH - 1:0] = $urandom;
+            data_i.a_in[EXP_WIDTH+MANTISSA_WIDTH - 1:MANTISSA_WIDTH] = $urandom_range(10, 60);
+            data_i.a_in[EXP_WIDTH+MANTISSA_WIDTH] = $urandom;
+            data_i.b_in[MANTISSA_WIDTH - 1:0] = $urandom;
+            data_i.b_in[EXP_WIDTH+MANTISSA_WIDTH - 1:MANTISSA_WIDTH] = $urandom_range(180, 230);
+            data_i.b_in[EXP_WIDTH+MANTISSA_WIDTH] = $urandom;
+            
+            super.insert_data();
+
+            @(data_i.done);
+
+            super.obtain_data();
+        end
+
+        repeat(5)//overflow
+        begin            
+            data_i = new();
+
+            data_i.a_in[MANTISSA_WIDTH - 1:0] = $urandom;
+            data_i.a_in[EXP_WIDTH+MANTISSA_WIDTH - 1:MANTISSA_WIDTH] = $urandom_range(180, 230);
+            data_i.a_in[EXP_WIDTH+MANTISSA_WIDTH] = $urandom;
+            data_i.b_in[MANTISSA_WIDTH - 1:0] = $urandom;
+            data_i.b_in[EXP_WIDTH+MANTISSA_WIDTH - 1:MANTISSA_WIDTH] = $urandom_range(10, 60);
+            data_i.b_in[EXP_WIDTH+MANTISSA_WIDTH] = $urandom;
+            
+            super.insert_data();
+
+            @(data_i.done);
+
+            super.obtain_data();
+        end
+    endtask : run
+endclass : ofuf_test
---
+++ floating_point_divider/testbench/tb_classes/tests/random_test.svh
@@ -0,0 +1,23 @@
+class random_test extends base_test;
+    task run;
+        super.run();
+      
+        repeat(10)
+        begin            
+            data_i = new();
+
+            data_i.a_in[MANTISSA_WIDTH - 1:0] = $urandom;
+            data_i.a_in[EXP_WIDTH+MANTISSA_WIDTH - 1:MANTISSA_WIDTH] = $urandom_range(117, 137);
+            data_i.a_in[EXP_WIDTH+MANTISSA_WIDTH] = $urandom;
+            data_i.b_in[MANTISSA_WIDTH - 1:0] = $urandom;
+            data_i.b_in[EXP_WIDTH+MANTISSA_WIDTH - 1:MANTISSA_WIDTH] = $urandom_range(117, 137);
+            data_i.b_in[EXP_WIDTH+MANTISSA_WIDTH] = $urandom;
+            
+            super.insert_data();
+
+            @(data_i.done);
+
+            super.obtain_data();
+        end
+    endtask : run
+endclass : random_test
